0bae8408affa8732e47c7b7e6821c4c0c1e05472
Queue Stainless <gce517+git@gmail.com>
2020-03-01 09:11:54 -0500

    Optimized imports

--- src/com/gce/controller/TextAnalyzerUIController.java
+++ src/com/gce/controller/TextAnalyzerUIController.java
@@ -1,21 +1,21 @@
 package com.gce.controller;
 
-import com.gce.TextAnalyzer;
 import com.gce.model.formValidation;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintStream;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
+import static com.gce.TextAnalyzer.*;
 
 public class TextAnalyzerUIController implements Initializable {
 
