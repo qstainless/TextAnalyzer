5dd71f6c3035e5767bf4584dd241d1b62f6acf1e
Queue Stainless <gce517+git@gmail.com>
2020-03-03 09:52:41 -0500

    Removed unneeded setters in model. Added class description.

--- src/com/gce/model/Word.java
+++ src/com/gce/model/Word.java
@@ -3,6 +3,10 @@ package com.gce.model;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
+/**
+ * Word model used to populate the text analysis results
+ * in a TableView.
+ */
 public class Word {
     private final SimpleIntegerProperty wordRankProperty;
     private final SimpleStringProperty wordContentProperty;
@@ -18,23 +22,11 @@ public class Word {
         return wordRankProperty.get();
     }
 
-    public void setWordRank(int wordRank) {
-        this.wordRankProperty.set(wordRank);
-    }
-
     public String getWordContent() {
         return wordContentProperty.get();
     }
 
-    public void setWordContent(String wordContent) {
-        this.wordContentProperty.set(wordContent);
-    }
-
     public int getWordFrequency() {
         return wordFrequencyProperty.get();
-    }
-
-    public void setWordFrequency(int wordFrequency) {
-        this.wordFrequencyProperty.set(wordFrequency);
     }
 }
