1ef150d28be0e47928c2f6fa19c4194490009358
Queue Stainless <gce517@gmail.com>
2020-03-17 11:03:06 -0400

    Create TextAnalyzerUIControllerTest.java

---
+++ src/com/gce/tests/TextAnalyzerUIControllerTest.java
@@ -0,0 +1,40 @@
+package com.gce.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TextAnalyzerUIControllerTest {
+
+    @Test
+    void initialize() {
+    }
+
+    @Test
+    void handleAnalyzeButtonAction() {
+    }
+
+    @Test
+    void analyzeUrl() {
+    }
+
+    @Test
+    void displayWords() {
+    }
+
+    @Test
+    void validateUrl() {
+    }
+
+    @Test
+    void fetchUrlContent() {
+    }
+
+    @Test
+    void countWordFrequencies() {
+    }
+
+    @Test
+    void sortWordsByFrequency() {
+    }
+}
